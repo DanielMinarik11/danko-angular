64838056ea09c305fdb5e798d6fd3b51ffefe1b2
DanielMinarik11 <145646443+DanielMinarik11@users.noreply.github.com>
2025-11-14 10:18:20 +0100

    gf

--- danko/src/main.ts
+++ danko/src/main.ts
@@ -1,6 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideZoneChangeDetection } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch(err => console.error(err));
+import { HomeComponent } from './app/home/home';
+import { QuestsComponent } from './app/quests/quests';
+import { QuestDetailComponent } from './app/quests/quest-detail/quest-detail';
+import { PlayersComponent } from './app/players/players';
+import { PlayerDetailComponent } from './app/players/player-detail/player-detail';
+import { ClansComponent } from './app/clans/clans';
+import { ClanDetailComponent } from './app/clans/clan-detail/clan-detail';
+
+const routes = [
+  { path: '', component: HomeComponent },
+  { path: 'quests', component: QuestsComponent },
+  { path: 'quests/:id', component: QuestDetailComponent },
+  { path: 'players', component: PlayersComponent },
+  { path: 'players/:id', component: PlayerDetailComponent },
+  { path: 'clans', component: ClansComponent },
+  { path: 'clans/:id', component: ClanDetailComponent },
+];
+
+bootstrapApplication(App, {
+  providers: [
+    provideZoneChangeDetection(),
+    provideRouter(routes)
+  ]
+});
